9c9fb90955e1f02353ef49d2744cbdce050242f6
Faraz Azadi <farazazadi70@gmail.com>
2024-07-12 23:01:08 +0330

    Add password field to user entity

--- DbContextInEfMigrations/Models/User.cs
+++ DbContextInEfMigrations/Models/User.cs
@@ -5,9 +5,14 @@ public class User
     public int Id { get; private set; }
 
     public string FullName { get; init; }
+    public string Password { get; private set; }
 
     public User(string fullName)
     {
         FullName = fullName;
     }
+
+
+    public void SetPassword(string password)
+        => Password = password;
 }
