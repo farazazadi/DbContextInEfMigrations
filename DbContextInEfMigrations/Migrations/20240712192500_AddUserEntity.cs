dc90667d61c79b94dd4bc2ac81594abfc4a5aad0
Faraz Azadi <farazazadi70@gmail.com>
2024-07-12 22:58:07 +0330

    Add User initial migraion

---
+++ DbContextInEfMigrations/Migrations/20240712192500_AddUserEntity.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbContextInEfMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                column: "FullName",
+                value: "Faraz");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
