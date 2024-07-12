28add22dcbfda49840271324a5a9ac791bbf5ff8
Faraz Azadi <farazazadi70@gmail.com>
2024-07-12 22:09:58 +0330

    Add UserModel

---
+++ DbContextInEfMigrations/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace DbContextInEfMigrations.Models;
+
+public class User
+{
+    public int Id { get; private set; }
+
+    public string FullName { get; init; }
+
+    public User(string fullName)
+    {
+        FullName = fullName;
+    }
+}
