00e4fdfe1361d9c2930cf48d10b02a6d7778547d
Faraz Azadi <farazazadi70@gmail.com>
2024-07-12 23:04:54 +0330

    Add UpdatePasswordOfUsers migration

---
+++ DbContextInEfMigrations/Migrations/20240712193138_UpdatePasswordOfUsers.cs
@@ -0,0 +1,37 @@
+﻿using DbContextInEfMigrations.Models;
+using DbContextInEfMigrations.Security;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbContextInEfMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePasswordOfUsers : Migration
+    {
+        private readonly MyDbContext _context;
+
+        
+        public UpdatePasswordOfUsers(MyDbContext context)
+        {
+            _context = context;
+        }
+
+
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            List<User> users = _context.Users.ToList();
+        
+            users.ForEach(user => user.SetPassword(user.FullName.GetHash()));
+
+            _context.SaveChanges();
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
